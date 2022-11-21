187199fbf83a0b2109a37bca6aa6708d2708333b
Kevin Neaton <kevin@neaton.xyz>
2022-11-21 16:10:27 -0500

    redirect landing page to mixtape

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/mixtape',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
