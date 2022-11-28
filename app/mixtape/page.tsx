4929fc044cfddd763fbf6ae5fb655cd7d5c973fe
Kevin Neaton <kevin@neaton.xyz>
2022-11-28 16:33:13 -0500

    link to zora

--- app/mixtape/page.tsx
+++ app/mixtape/page.tsx
@@ -1,4 +1,5 @@
 import P9Window from '../../components/P9Window'
+import Link from 'next/link'
 
 export default function About() {
   return (
@@ -6,6 +7,8 @@ export default function About() {
     <div className="font-display text-[28px] p-[40px]">
       <p>CC0lab Mixtape Vol.1</p>
       <p>11/28/2022</p>
+      <br />
+      {'{'}<Link href="https://create.zora.co/collections/0x72529ca1ca1be6657cfc9f9f12c614e2fbd8d761">Mint on Zora</Link>{'}'}
     </div>
   </P9Window>
   )
