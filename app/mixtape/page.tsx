d4c7cdc853e764c6a13dd3989e86fac7f1f345a3
Kevin Neaton <kevin@neaton.xyz>
2023-02-07 21:28:56 -0500

    add the stems and link to remix page

--- app/mixtape/page.tsx
+++ app/mixtape/page.tsx
@@ -6,6 +6,7 @@
 // and related and neighboring rights to this software to the public domain
 // worldwide. This software is distributed without any warranty.
 
+import Link from 'next/link'
 import P9Window from '../../components/P9Window'
 import Providers from './providers'
 import DropsMinter from '../../components/DropsMinter'
@@ -16,12 +17,16 @@ export default function Page() {
     <div className="grid gap-[10px] md:gap-[20px] auto-cols-[minmax(0,auto)] auto-rows-[minmax(0,auto)] row-start-2 justify-self-center justify-items-center items-center h-full z-10 pointer-events-none">
       <Providers>
         <P9Window className="w-[480px] max-w-full max-h-full pointer-events-auto">
-            <DropsMinter />            
-          </P9Window>
-          
-          <P9Window className="w-full md:w-auto md:col-start-2 max-h-full pointer-events-auto">
-            <DropsDescription />
-          </P9Window>
+          <DropsMinter />            
+        </P9Window>
+        
+        <P9Window className="w-full md:w-auto md:col-start-2 max-h-full pointer-events-auto">
+          <div className="bg-green font-display text-[20px] p-[10px]">
+            <p className="max-w-[20em]">We did it! Check out the <Link className="braces" href="remix">STEMS</Link> and join the <span className="font-displayUnderscored">remix cooperation</span>.
+            </p>
+          </div>
+          <DropsDescription />
+        </P9Window>
   
       </Providers>
     </div>
--- app/remix/page.tsx
+++ app/remix/page.tsx
@@ -13,8 +13,15 @@ export default function Page() {
   return (
      <P9Window className="row-start-2 justify-self-center self-center max-h-full z-10">
       <div className="flex flex-col gap-[1.5em] font-display text-[16px] p-[10px] max-w-[40em] min-h-[26.667em]">
-      <p>We have made the stems from all songs publicly available for downloading right here! Check them out and join the remix cooperation. Use these stems to make a remix, submit before Feb 24th and the CC0 crew will pick their favorites to publish on Songcamp's record store: Present Materials (with permission from the remix makers).</p>
-      <p className="italic">This page will be updated shortly to include download links to ALL of our stems. Please come back later to download them. In the meantime, most of our stems can be found under the <ArpeggiLink className="braces" slug="0x8815C5Cf745a8fCdFE134dA299cBF6f3012E2674/sounds">CC0lab</ArpeggiLink> profile on Arpeggi.</p>
+      <h3 className="font-displayUnderscored text-[24px]">Mixtape Vol. 1 Remix Cooperation</h3>
+      <p>We have made the stems from all songs publicly available for downloading right here! Check them out and join the remix cooperation.</p>
+      <h4 className="font-display text-[20px]">How to COOP:</h4>
+      <p>Use these stems to make a remix, submit before Feb 24th and the CC0 crew will pick their favorites to publish on Songcamp's record store: Present Materials (with permission from the remix makers).</p>
+      <h4 className="font-display text-[20px]">Get the STEMS:</h4>
+      <ul>
+        <li>Download <a className="braces" rel="noreferrer noopener" href="https://arweave.net/ZlBOLKz_rIsrGx93PrfN_zfw4RFMe4HantbdLAZ4psI">DIRECT</a> from Arweave.</li>
+        <li>Remix <ArpeggiLink className="braces" slug="0x8815C5Cf745a8fCdFE134dA299cBF6f3012E2674/sounds">CC0lab</ArpeggiLink> on Arpeggi.</li>
+      </ul> 
       </div>
     </P9Window>
   )
