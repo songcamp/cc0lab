0edf76ef2e9fa859de74a771d5120d02b6e30486
Kevin Neaton <kevin@neaton.xyz>
2023-03-13 10:12:21 -0400

    fix link to mirror article

--- app/remix/page.tsx
+++ app/remix/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
      <P9Window className="row-start-2 justify-self-center self-center max-h-full z-10">
       <div className="flex flex-col gap-[1.5em] font-display text-[16px] p-[10px] max-w-[40em] min-h-[26.667em]">
       <h3 className="font-displayUnderscored text-[24px]">Mixtape Vol. 1 Remix Cooperation</h3>
-      <p>We've officially launched a remix cooperation! Read the full announcement on <a className="braces" rel="noreferrer noopener" href="https://songcamp.mirror.xyz/c3Y6ABdmdAeayp1Q-Mh4Oom6pJlZ_fGBid4pbrvKpfI">Mirror</a> and join here.</p>
+      <p>We've officially launched a remix cooperation! Read the full announcement on <a className="braces" rel="noreferrer noopener" href="https://songcamp.mirror.xyz/c2Y6ABdmdAeayp1Q-Mh4Oom6pJlZ_fGBid4pbrvKpfI">Mirror</a> and join here.</p>
       <h4 className="font-display text-[20px]">How to COOP:</h4>
       <ol className="list-inside list-decimal">
       <li><span className="font-displayUnderscored">Get the stems</span> <a className="braces" rel="noreferrer noopener" href="https://arweave.net/ZlBOLKz_rIsrGx93PrfN_zfw4RFMe4HantbdLAZ4psI">HERE</a> or remix <ArpeggiLink className="braces" slug="0x8815C5Cf745a8fCdFE134dA299cBF6f3012E2674/sounds">CC0lab</ArpeggiLink> on Apreggi.</li>
