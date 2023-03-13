c127ee760d3a9dbb3c8f6651b8ad3e3c3671f49a
Kevin Neaton <kevin@neaton.xyz>
2023-03-12 23:51:12 -0400

    update remix page

--- app/remix/page.tsx
+++ app/remix/page.tsx
@@ -8,20 +8,20 @@
 
 import P9Window from '../../components/P9Window'
 import ArpeggiLink from '../../components/ArpeggiLink'
+import PresentMaterialLink from '../../components/PresentMaterialLink'
 
 export default function Page() {
   return (
      <P9Window className="row-start-2 justify-self-center self-center max-h-full z-10">
       <div className="flex flex-col gap-[1.5em] font-display text-[16px] p-[10px] max-w-[40em] min-h-[26.667em]">
       <h3 className="font-displayUnderscored text-[24px]">Mixtape Vol. 1 Remix Cooperation</h3>
-      <p>We have made the stems from all songs publicly available for downloading right here! Check them out and join the remix cooperation.</p>
+      <p>We've officially launched a remix cooperation! Read the full announcement on <a className="braces" rel="noreferrer noopener" href="https://songcamp.mirror.xyz/c3Y6ABdmdAeayp1Q-Mh4Oom6pJlZ_fGBid4pbrvKpfI">Mirror</a> and join here.</p>
       <h4 className="font-display text-[20px]">How to COOP:</h4>
-      <p>Use these stems to make a remix, submit before Feb 24th and the CC0 crew will pick their favorites to publish on Songcamp's record store: Present Materials (with permission from the remix makers).</p>
-      <h4 className="font-display text-[20px]">Get the STEMS:</h4>
-      <ul>
-        <li>Download <a className="braces" rel="noreferrer noopener" href="https://arweave.net/ZlBOLKz_rIsrGx93PrfN_zfw4RFMe4HantbdLAZ4psI">DIRECT</a> from Arweave.</li>
-        <li>Remix <ArpeggiLink className="braces" slug="0x8815C5Cf745a8fCdFE134dA299cBF6f3012E2674/sounds">CC0lab</ArpeggiLink> on Arpeggi.</li>
-      </ul> 
+      <ol className="list-inside list-decimal">
+      <li><span className="font-displayUnderscored">Get the stems</span> <a className="braces" rel="noreferrer noopener" href="https://arweave.net/ZlBOLKz_rIsrGx93PrfN_zfw4RFMe4HantbdLAZ4psI">HERE</a> or remix <ArpeggiLink className="braces" slug="0x8815C5Cf745a8fCdFE134dA299cBF6f3012E2674/sounds">CC0lab</ArpeggiLink> on Apreggi.</li>
+      <li><span className="font-displayUnderscored">Submit your remixes</span> <a className="braces" rel="noreferrer noopener" href="https://docs.google.com/forms/d/e/1FAIpQLSe6LDfIBcU2eoREhQ5MR-10CbeXkV6YHteNxtHeLsZLaiEB1g/viewform">HERE</a> by April 1st.</li>
+      </ol>
+      <p>The CC0 crew will mint selected remixes and publish them on <PresentMaterialLink className="braces">Present Material</PresentMaterialLink> with a 70/30 split between you and the CC0lab artists.</p>
       </div>
     </P9Window>
   )
